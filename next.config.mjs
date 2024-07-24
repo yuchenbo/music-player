395aa2772183649d15f692be7711f29d241c694d
stephen <stephen@1024.tt>
2024-07-24 15:53:34 +0800

    feat(): 初始化脚手架

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+};
 
 export default nextConfig;
